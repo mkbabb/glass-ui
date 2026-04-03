54473ec0826dfa7e6834c2ca9cbab47267cef8b2
Mike Babb <mike7400@gmail.com>
2026-04-03 14:48:10 -0400

    feat: add TabsIndicator component with spring-snappy animated pill

---
+++ src/components/ui/tabs/TabsIndicator.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from "vue";
+import { TabsIndicator, type TabsIndicatorProps } from "reka-ui";
+import { cn } from "../../../utils";
+
+const props = defineProps<TabsIndicatorProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props;
+    return delegated;
+});
+</script>
+
+<template>
+    <TabsIndicator
+        v-bind="delegatedProps"
+        :class="cn(
+            'absolute left-0 h-[--reka-tabs-indicator-size] translate-x-[--reka-tabs-indicator-position] rounded-pill bg-secondary/80 transition-[width,transform] duration-[var(--duration-normal)] ease-[var(--spring-snappy)]',
+            props.class,
+        )"
+    />
+</template>
